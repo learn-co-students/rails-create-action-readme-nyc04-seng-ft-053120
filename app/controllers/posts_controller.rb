8ae859d19efb046ee99dd5511492b14db03f048f
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-07-20 16:52:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,12 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+  def create
+    @post = Post.new #creates a new post
+    @post.title = params[:title] #stores user's title input as post.title
+    @post.description = params[:description] #stores user's desc input as post.description
+    @post.save #saves 
+    redirect_to post_path(@post)
+  end
+
 end
